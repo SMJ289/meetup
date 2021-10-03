05bc38a23491dc42be14e0048049108823ad0b23
Steven Meyers <sjjmeyers@gmail.com>
2021-10-03 14:12:03 -0500

    Modify registration form to use query params

--- force-app/main/default/lwc/meetupRegistrationFormTwo/meetupRegistrationFormTwo.js
+++ force-app/main/default/lwc/meetupRegistrationFormTwo/meetupRegistrationFormTwo.js
@@ -1,10 +1,26 @@
-import { api, LightningElement, track } from 'lwc';
+import { api, LightningElement, track, wire } from 'lwc';
 import saveRegistration from '@salesforce/apex/MeetupRegistrationController.saveRegistration';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
-import {NavigationMixin} from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
 
 
 export default class insertRecordCustomObjectLwc extends NavigationMixin (LightningElement) {
+    currentPageReference = null;
+    urlStateParameters = null;
+    urlCode = null;
+    
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+       if (currentPageReference) {
+          this.urlStateParameters = currentPageReference.state;
+          this.setParametersBasedOnUrl();
+       }
+    }
+ 
+    setParametersBasedOnUrl() {
+       this.urlCode = this.urlStateParameters.c__code || null;
+       this.registrationObjCode = this.urlCode;
+    }
 
     @track registrationObjFirstName;
     @track registrationObjLastName;
@@ -14,9 +30,10 @@ export default class insertRecordCustomObjectLwc extends NavigationMixin (Lightn
     @track errorMsg;
 
    registrationHandleChange(event){
-        if(event.target.name == 'registrationFirstName'){
+      if(event.target.name == 'registrationFirstName'){
         this.registrationObjFirstName = event.target.value;  
         }
+
       if(event.target.name == 'registrationLastName'){
         this.registrationObjLastName = event.target.value;  
       }
@@ -24,9 +41,9 @@ export default class insertRecordCustomObjectLwc extends NavigationMixin (Lightn
       if(event.target.name == 'registrationEmail'){
         this.registrationObjEmail = event.target.value;  
       }
-      if(event.target.name == 'registrationCode'){
+      /*if(event.target.name == 'registrationCode'){
         this.registrationObjCode = event.target.value;  
-      }
+      }*/
  }
 
  submitAction(){
@@ -50,6 +67,7 @@ export default class insertRecordCustomObjectLwc extends NavigationMixin (Lightn
             },
          });
     })
+
     .catch(error =>{
       const toastEvent = new ShowToastEvent({
         title:'Oops!',
